45a73ab03522cb54ebaab7defd2c73e5079432e1
emmanuel <oliveiravianaviana@gmail.com>
2021-02-16 15:04:03 -0300

    commit js

--- scripts.js
+++ scripts.js
@@ -46,13 +46,26 @@ const transactions = [
 
 const Transaction = {
     incomes () {
+        let income = 0;
+        transactions.forEach(transaction => {
+            if(transaction.amount > 0) {
+                income += transaction.amount;
+            }
+        })
         //somar as entradas
+        return income
     },
     expenses() {
-        //somar as saídas
+        let expense = 0;
+        transactions.forEach(transaction => {
+            if(transaction.amount < 0) {
+                expense += transaction.amount;
+            }
+        })
+                return expense
     },
     total () {
-        // entradas - saídas
+        return Transaction.incomes() + Transaction.expenses();
     }
 }
 
@@ -75,22 +88,47 @@ const DOM = {
         const html = 
            `
              <td class="description">${transaction.description}</td>
-             <td class="${CSSclass}">${transaction.amount}</td>
+             <td class="${CSSclass}">${amount}</td>
              <td class="date">${transaction.date}</td>
              <td>
                 <img src="./assets/minus.svg" alt="Remover Transação">
              </td>
            `
         return html
+    },
+
+    updateBalance() {
+        document
+        .getElementById('incomeDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document
+        .getElementById('expenseDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document
+        .getElementById('totalDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.total())
     }
 }
 
 const Utils = {
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        return signal + value
     }
 }
 
 transactions.forEach(function(transaction) {
     DOM.addTransaction(transaction)
 })
+
+DOM.updateBalance()
